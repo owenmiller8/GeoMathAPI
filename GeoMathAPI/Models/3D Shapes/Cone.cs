2a8fc9cff12cbc977072bc6ae0684dfb1475c3b0
Owen Miller <owen.miller@fidelity-energy.co.uk>
2020-03-12 11:43:59 +0000

    Added a couple more 3D shapes and made a few more of the methods public and static

---
+++ GeoMathAPI/Models/3D Shapes/Cone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoMathAPI.Models._3D_Shapes
+{
+    public class Cone
+    {
+        public double SurfaceArea { get; set; }
+
+        public double Volume { get; set; }
+
+        public Cone(double radius, double height, _CalcTypes calc)
+        {
+            switch (calc)
+            {
+                case _CalcTypes.AreaCalc:
+                    SurfaceArea = CalculateArea(radius, height);
+                    break;
+                case _CalcTypes.VolumeCalc:
+                    Volume = CalculateVolume(radius, height);
+                    break;
+                default:
+                    //Perhaps log an issue here?
+                    break;
+            }
+        }
+
+        private double CalculateArea(double radius, double height)
+        {
+            return Math.Round(Math.PI * radius * (radius + Triangle.CalculateHypotenuse(radius, height)), 3);
+        }
+
+        private double CalculateVolume(double radius, double length)
+        {
+            return Math.Round((1 / 3) * Circle.CalculateArea(radius) * length, 3);
+        }
+    }
+}
---
+++ GeoMathAPI/Models/3D Shapes/Pyramid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoMathAPI.Models._3D_Shapes
+{
+    public class Pyramid
+    {
+        public double SurfaceArea { get; set; }
+
+        public double Volume { get; set; }
+
+        public Pyramid(double height, double width, double length, _CalcTypes calc, _3DBases shapeBase)
+        {
+            switch (calc)
+            {
+                case _CalcTypes.AreaCalc:
+                    SurfaceArea = CalculateArea(height, width, length, shapeBase);
+                    break;
+                case _CalcTypes.VolumeCalc:
+                    Volume = CalculateVolume(height, width, length, shapeBase);
+                    break;
+                default:
+                    //Perhaps log an issue here?
+                    break;
+            }
+        }
+
+        private double CalculateArea(double height, double width, double length, _3DBases b)
+        {
+            switch (b)
+            {
+                case _3DBases.Square:
+                    return Math.Round(Square.CalculateArea(height)  + (0.5 * Square.CalculatePerimeter(length) * Triangle.CalculateHypotenuse(height, 0.5f * width)), 3);
+                case _3DBases.Triangle:
+                    return Math.Round(Triangle.CalculateArea(height, width) + (0.5 * Triangle.CalculatePerimeter(width, length) * Triangle.CalculateHypotenuse(width, height)), 3);
+                default:
+                    return 123456f;
+            }
+        }
+
+        private double CalculateVolume(double height, double width, double length, _3DBases b)
+        {
+            switch (b)
+            {
+                case _3DBases.Square:
+                    return Math.Round((1 / 3) * Square.CalculateArea(height) * height, 3);
+                case _3DBases.Triangle:
+                    return Math.Round((1 / 3) * Triangle.CalculateArea(length, width) * height, 3);
+                default:
+                    return 123456f;
+            }
+        }
+    }
+}
